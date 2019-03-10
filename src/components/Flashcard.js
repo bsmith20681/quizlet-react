3173476bc775c3729b20386be0a55a0828a1039f
Test <bsmith20681@gmail.com>
2019-03-10 15:26:38 -0600

    added rotating flash card on click

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -4,11 +4,28 @@ import React, { Component } from 'react'
 
 class Flashcard extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      isFlipped: false
+    }
+  }
+
+  handleToggle(e){
+    e.preventDefault();
+    this.setState({
+      isFlipped: !this.state.isFlipped
+    })
+  }
+
+
+
   render() {
+    let {isFlipped} = this.state;
     return (
     <div className="row">
       <div className="col-lg-12 d-flex justify-content-center">
-        <div className="flashcard">
+        <div className={`flashcard ${isFlipped ? 'flip' : ''}`} onClick={(e) => this.handleToggle(e)}>
           <div className="flashcard__side flashcard__side--front">
               <h1 id="flashcard__front" className="text-center m-3"> front</h1>
             </div>
